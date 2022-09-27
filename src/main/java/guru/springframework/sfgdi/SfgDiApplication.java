111152ac4ab1658aeb928ecfb81b833615fc8bd4
Bainbridge <jack.bainbridge@medavie.bluecross.ca>
2022-09-27 15:50:18 -0300

    Adding a new Spring Injected Controlle called: MyController

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -1,13 +1,22 @@
 package guru.springframework.sfgdi;
 
+import guru.springframework.sfgdi.Controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SfgDiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SfgDiApplication.class, args);
+
+		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
+
+		// Retrieve our newly created bean from the Spring Context:
+		MyController myController = (MyController) ctx.getBean("myController");
+
+		String greeting = myController.sayHello();
+		System.out.printf(greeting + "\n");
 	}
 
 }
